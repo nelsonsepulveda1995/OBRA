83911dfd21ed0d8b6f8a397671dd7f5c72c0c288
david ruiz <elchetodelciber_95@hotmail.com>
2019-06-17 14:59:43 -0300

    Cambios en el front

--- Front/Forms/Users/Compras/Acciones/Gestionar Provedores/GestionarProvedores.Designer.cs
+++ Front/Forms/Users/Compras/Acciones/Gestionar Provedores/GestionarProvedores.Designer.cs
@@ -37,12 +37,10 @@
             this.materialLabel3 = new MaterialSkin.Controls.MaterialLabel();
             this.materialLabel4 = new MaterialSkin.Controls.MaterialLabel();
             this.materialLabel5 = new MaterialSkin.Controls.MaterialLabel();
-            this.materialLabel6 = new MaterialSkin.Controls.MaterialLabel();
             this.materialSingleLineTextField2 = new MaterialSkin.Controls.MaterialSingleLineTextField();
             this.materialSingleLineTextField3 = new MaterialSkin.Controls.MaterialSingleLineTextField();
             this.materialSingleLineTextField4 = new MaterialSkin.Controls.MaterialSingleLineTextField();
             this.materialSingleLineTextField5 = new MaterialSkin.Controls.MaterialSingleLineTextField();
-            this.materialSingleLineTextField6 = new MaterialSkin.Controls.MaterialSingleLineTextField();
             this.SuspendLayout();
             // 
             // label1
@@ -97,9 +95,9 @@
             this.materialLabel1.Location = new System.Drawing.Point(102, 123);
             this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel1.Name = "materialLabel1";
-            this.materialLabel1.Size = new System.Drawing.Size(108, 19);
+            this.materialLabel1.Size = new System.Drawing.Size(42, 19);
             this.materialLabel1.TabIndex = 3;
-            this.materialLabel1.Text = "materialLabel1";
+            this.materialLabel1.Text = "CUIT";
             // 
             // materialSingleLineTextField1
             // 
@@ -128,9 +126,9 @@
             this.materialLabel2.Location = new System.Drawing.Point(102, 169);
             this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel2.Name = "materialLabel2";
-            this.materialLabel2.Size = new System.Drawing.Size(108, 19);
+            this.materialLabel2.Size = new System.Drawing.Size(63, 19);
             this.materialLabel2.TabIndex = 5;
-            this.materialLabel2.Text = "materialLabel2";
+            this.materialLabel2.Text = "Nombre";
             // 
             // materialLabel3
             // 
@@ -141,9 +139,9 @@
             this.materialLabel3.Location = new System.Drawing.Point(102, 219);
             this.materialLabel3.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel3.Name = "materialLabel3";
-            this.materialLabel3.Size = new System.Drawing.Size(108, 19);
+            this.materialLabel3.Size = new System.Drawing.Size(73, 19);
             this.materialLabel3.TabIndex = 6;
-            this.materialLabel3.Text = "materialLabel3";
+            this.materialLabel3.Text = "Direccion";
             // 
             // materialLabel4
             // 
@@ -154,9 +152,9 @@
             this.materialLabel4.Location = new System.Drawing.Point(102, 266);
             this.materialLabel4.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel4.Name = "materialLabel4";
-            this.materialLabel4.Size = new System.Drawing.Size(108, 19);
+            this.materialLabel4.Size = new System.Drawing.Size(69, 19);
             this.materialLabel4.TabIndex = 7;
-            this.materialLabel4.Text = "materialLabel4";
+            this.materialLabel4.Text = "Telefono";
             // 
             // materialLabel5
             // 
@@ -167,22 +165,9 @@
             this.materialLabel5.Location = new System.Drawing.Point(102, 315);
             this.materialLabel5.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel5.Name = "materialLabel5";
-            this.materialLabel5.Size = new System.Drawing.Size(108, 19);
+            this.materialLabel5.Size = new System.Drawing.Size(136, 19);
             this.materialLabel5.TabIndex = 8;
-            this.materialLabel5.Text = "materialLabel5";
-            // 
-            // materialLabel6
-            // 
-            this.materialLabel6.AutoSize = true;
-            this.materialLabel6.Depth = 0;
-            this.materialLabel6.Font = new System.Drawing.Font("Roboto", 11F);
-            this.materialLabel6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.materialLabel6.Location = new System.Drawing.Point(102, 368);
-            this.materialLabel6.MouseState = MaterialSkin.MouseState.HOVER;
-            this.materialLabel6.Name = "materialLabel6";
-            this.materialLabel6.Size = new System.Drawing.Size(108, 19);
-            this.materialLabel6.TabIndex = 9;
-            this.materialLabel6.Text = "materialLabel6";
+            this.materialLabel5.Text = "Correo Electronico";
             // 
             // materialSingleLineTextField2
             // 
@@ -256,35 +241,15 @@
             this.materialSingleLineTextField5.Text = "materialSingleLineTextField5";
             this.materialSingleLineTextField5.UseSystemPasswordChar = false;
             // 
-            // materialSingleLineTextField6
-            // 
-            this.materialSingleLineTextField6.Depth = 0;
-            this.materialSingleLineTextField6.Hint = "";
-            this.materialSingleLineTextField6.Location = new System.Drawing.Point(344, 364);
-            this.materialSingleLineTextField6.MaxLength = 32767;
-            this.materialSingleLineTextField6.MouseState = MaterialSkin.MouseState.HOVER;
-            this.materialSingleLineTextField6.Name = "materialSingleLineTextField6";
-            this.materialSingleLineTextField6.PasswordChar = '\0';
-            this.materialSingleLineTextField6.SelectedText = "";
-            this.materialSingleLineTextField6.SelectionLength = 0;
-            this.materialSingleLineTextField6.SelectionStart = 0;
-            this.materialSingleLineTextField6.Size = new System.Drawing.Size(250, 23);
-            this.materialSingleLineTextField6.TabIndex = 14;
-            this.materialSingleLineTextField6.TabStop = false;
-            this.materialSingleLineTextField6.Text = "materialSingleLineTextField6";
-            this.materialSingleLineTextField6.UseSystemPasswordChar = false;
-            // 
             // GestionarProvedores
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(790, 557);
-            this.Controls.Add(this.materialSingleLineTextField6);
             this.Controls.Add(this.materialSingleLineTextField5);
             this.Controls.Add(this.materialSingleLineTextField4);
             this.Controls.Add(this.materialSingleLineTextField3);
             this.Controls.Add(this.materialSingleLineTextField2);
-            this.Controls.Add(this.materialLabel6);
             this.Controls.Add(this.materialLabel5);
             this.Controls.Add(this.materialLabel4);
             this.Controls.Add(this.materialLabel3);
@@ -313,11 +278,9 @@
         private MaterialSkin.Controls.MaterialLabel materialLabel3;
         private MaterialSkin.Controls.MaterialLabel materialLabel4;
         private MaterialSkin.Controls.MaterialLabel materialLabel5;
-        private MaterialSkin.Controls.MaterialLabel materialLabel6;
         private MaterialSkin.Controls.MaterialSingleLineTextField materialSingleLineTextField2;
         private MaterialSkin.Controls.MaterialSingleLineTextField materialSingleLineTextField3;
         private MaterialSkin.Controls.MaterialSingleLineTextField materialSingleLineTextField4;
         private MaterialSkin.Controls.MaterialSingleLineTextField materialSingleLineTextField5;
-        private MaterialSkin.Controls.MaterialSingleLineTextField materialSingleLineTextField6;
     }
 }
